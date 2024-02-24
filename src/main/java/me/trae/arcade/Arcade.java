01de81b75110bf6f7257a94a1acff750003965c1
TR4E <trae.rohleder@outlook.com>
2024-02-25 01:08:38 +1100

    Updated Spigot Framework

--- src/main/java/me/trae/arcade/Arcade.java
+++ src/main/java/me/trae/arcade/Arcade.java
@@ -1,9 +1,13 @@
 package me.trae.arcade;
 
-import me.trae.arcade.effect.EffectManager;
 import me.trae.arcade.game.GameManager;
 import me.trae.arcade.lobby.LobbyManager;
-import me.trae.arcade.scoreboard.ArcadeScoreboard;
+import me.trae.champions.Champions;
+import me.trae.champions.blood.BloodManager;
+import me.trae.champions.build.BuildManager;
+import me.trae.champions.role.RoleManager;
+import me.trae.champions.skill.SkillManager;
+import me.trae.core.Core;
 import me.trae.core.chat.ChatManager;
 import me.trae.core.client.ClientManager;
 import me.trae.core.command.CommandManager;
@@ -13,6 +17,7 @@ import me.trae.core.database.DatabaseManager;
 import me.trae.core.death.DeathManager;
 import me.trae.core.donation.global.GlobalDonationManager;
 import me.trae.core.donation.local.LocalDonationManager;
+import me.trae.core.effect.EffectManager;
 import me.trae.core.framework.SpigotPlugin;
 import me.trae.core.gamer.global.GlobalGamerManager;
 import me.trae.core.item.ItemManager;
@@ -24,35 +29,67 @@ import me.trae.core.redis.RedisManager;
 import me.trae.core.scoreboard.ScoreboardManager;
 import me.trae.core.server.ServerManager;
 import me.trae.core.updater.UpdaterManager;
+import me.trae.core.weapon.WeaponManager;
+import me.trae.core.world.WorldManager;
 
 public class Arcade extends SpigotPlugin {
 
     @Override
     public void registerManagers() {
-        // Core
-        addManager(new ChatManager(this));
-        addManager(new ClientManager(this));
-        addManager(new CommandManager(this));
-        addManager(new ConfigManager(this));
-        addManager(new CountdownManager(this));
-        addManager(new DatabaseManager(this));
-        addManager(new DeathManager(this));
-        addManager(new GlobalDonationManager(this));
-        addManager(new LocalDonationManager(this));
-        addManager(new GlobalGamerManager(this));
-        addManager(new ItemManager(this));
-        addManager(new MenuManager(this));
-        addManager(new NetworkManager(this));
-        addManager(new PlayerManager(this));
-        addManager(new RechargeManager(this));
-        addManager(new RedisManager(this));
-        addManager(new ScoreboardManager(this, ArcadeScoreboard.class));
-        addManager(new ServerManager(this));
-        addManager(new UpdaterManager(this));
-
-        // Arcade
-        addManager(new EffectManager(this));
         addManager(new GameManager(this));
         addManager(new LobbyManager(this));
+    }
+
+    @Override
+    public void registerFactories() {
+        addFactory(new Core() {
+            @Override
+            public boolean loadOnStart() {
+                return true;
+            }
+
+            @Override
+            public void registerManagers() {
+                addManager(ChatManager.class);
+                addManager(ClientManager.class);
+                addManager(CommandManager.class);
+                addManager(ConfigManager.class);
+                addManager(CountdownManager.class);
+                addManager(DatabaseManager.class);
+                addManager(DeathManager.class);
+                addManager(GlobalDonationManager.class);
+                addManager(LocalDonationManager.class);
+                addManager(EffectManager.class);
+                addManager(GlobalGamerManager.class);
+                addManager(ItemManager.class);
+                addManager(MenuManager.class);
+                addManager(NetworkManager.class);
+                addManager(PlayerManager.class);
+                addManager(RechargeManager.class);
+                addManager(RedisManager.class);
+                addManager(ScoreboardManager.class);
+                addManager(ServerManager.class);
+                addManager(UpdaterManager.class);
+                addManager(WeaponManager.class);
+                addManager(WorldManager.class);
+            }
+        });
+
+        addFactory(new Champions() {
+            @Override
+            public boolean loadOnStart() {
+                return true;
+            }
+
+            @Override
+            public void registerManagers() {
+                addManager(BloodManager.class);
+                addManager(BuildManager.class);
+                addManager(me.trae.champions.effect.EffectManager.class);
+                addManager(RoleManager.class);
+                addManager(SkillManager.class);
+                addManager(me.trae.champions.weapon.WeaponManager.class);
+            }
+        });
     }
 }
--- src/main/java/me/trae/arcade/utility/UtilChampions.java
+++ src/main/java/me/trae/arcade/utility/UtilChampions.java
@@ -1,44 +1,20 @@
 package me.trae.arcade.utility;
 
-import me.trae.champions.blood.BloodManager;
-import me.trae.champions.build.BuildManager;
-import me.trae.champions.role.RoleManager;
-import me.trae.champions.skill.SkillManager;
-import me.trae.core.framework.SpigotManager;
+import me.trae.champions.Champions;
 import me.trae.core.framework.SpigotPlugin;
 import me.trae.core.utility.UtilPlugin;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class UtilChampions {
 
     public static void initialize() {
         final SpigotPlugin instance = UtilPlugin.getInstance();
 
-        for (final Class<? extends SpigotManager> clazz : getManagerClasses()) {
-            try {
-                instance.addManager(clazz.getConstructor(SpigotPlugin.class).newInstance(instance));
-            } catch (final Exception e) {
-                e.printStackTrace();
-            }
-        }
+        instance.getFactoryByClass(Champions.class).initialize(instance);
     }
 
     public static void shutdown() {
         final SpigotPlugin instance = UtilPlugin.getInstance();
 
-        for (final Class<? extends SpigotManager> clazz : getManagerClasses()) {
-            final SpigotManager manager = instance.getManagerByClass(clazz);
-            if (manager == null) {
-                continue;
-            }
-
-            instance.removeManager(manager);
-        }
-    }
-
-    private static List<Class<? extends SpigotManager>> getManagerClasses() {
-        return Arrays.asList(BloodManager.class, BuildManager.class, RoleManager.class, SkillManager.class);
+        instance.getFactoryByClass(Champions.class).shutdown(instance);
     }
 }
